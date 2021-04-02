eabae6e9b24bea796374fc71824865a6bd387e73
Lukas Gjetting <lukasgjetting@gmail.com>
2021-04-02 17:01:02 +0200

    Set up ESLint

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,35 @@ import { ArchiveGamesResponse, ArchivesListResponse, Game } from './types';
 const API_URL = 'https://api.chess.com/pub';
 
 const sendRequest = async (endpoint: string) => {
-    try {
-        const response = await fetch(`${API_URL}${endpoint}`);
+	try {
+		const response = await fetch(`${API_URL}${endpoint}`);
 
-        return response.json();
-    } catch (e) {
-        console.error(`Error for ${endpoint}`, e);
-    }
+		return await response.json();
+	} catch (e) {
+		console.error(`Error for ${endpoint}`, e);
+	}
 };
 
 const getGames = async (username: string) => {
-    const response: ArchivesListResponse = await sendRequest(`/player/${username}/games/archives`);
-    const endpoints = response.archives
-        .map((url) => url.replace(API_URL, ''))
-        // Reverse so they are in reverse chronological order
-        .reverse();
+	const response: ArchivesListResponse = await sendRequest(`/player/${username}/games/archives`);
+	const endpoints = response.archives
+		.map((url) => url.replace(API_URL, ''))
+	// Reverse so they are in reverse chronological order
+		.reverse();
 
-    let games: Game[] = [];
+	let games: Game[] = [];
 
-    for (const endpoint of endpoints) {
-        const gamesResponse: ArchiveGamesResponse = await sendRequest(endpoint);
+	// ESLint doesn't like using async loops
+	// However, we need the requests to not run in parallel, due to Chess.com rate limits
+	/* eslint-disable no-restricted-syntax */
+	for (const endpoint of endpoints) {
+		const gamesResponse: ArchiveGamesResponse = await sendRequest(endpoint);
 
-        games = [...games, ...gamesResponse.games];
-    }
+		games = [...games, ...gamesResponse.games];
+	}
+	/* eslint-enable no-restricted-syntax */
 
-    return games;
+	return games;
 };
 
-getGames('lukasgjetting').then(console.log)
+getGames('lukasgjetting').then(console.log);
