83e1532d81c140bbf50bc359f3f3706675e76d0e
Lukas Gjetting <lukasgjetting@gmail.com>
2021-04-02 17:50:31 +0200

    Implement a few game util functions

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ const sendRequest = async (endpoint: string) => {
 		return await response.json();
 	} catch (e) {
 		console.error(`Error for ${endpoint}`, e);
+
+		throw e;
 	}
 };
 
@@ -35,4 +37,51 @@ const getGames = async (username: string) => {
 	return games;
 };
 
-getGames('lukasgjetting').then(console.log);
+const getGameLength = (game: Game): number => {
+	const match = game.pgn.match(/[0-9]+\. /g);
+
+	if (match == null) {
+		return 0;
+	}
+
+	const lastMatch = match[match.length - 1];
+
+	return Number(lastMatch.replace('. ', ''));
+};
+
+// Returns 1, 0.5 or 0, based on the result of the game
+// From the perspective of the given username
+const getGameResultScore = (game: Game, username: string): number => {
+	// If neither player won, it must have been drawn
+	if (game.black.result !== 'win' && game.white.result !== 'win') {
+		return 0.5;
+	}
+
+	if (
+		(game.black.result === 'win' && game.black.username === username) ||
+        (game.white.result === 'win' && game.white.username === username)
+	) {
+		return 1;
+	}
+
+	return 0;
+};
+
+const main = async () => {
+	const username = 'lukasgjetting';
+
+	const games = await getGames(username);
+
+	const gamesLength = games.map(getGameLength);
+	const gamesScore = games.map((g) => getGameResultScore(g, username));
+
+	console.log(games.reduce<{ moves: number, score: number }[]>((arr, game, index) => [
+		...arr,
+		{
+			moves: gamesLength[index],
+			score: gamesScore[index],
+		},
+	], []));
+};
+
+main();
--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,37 @@
 export interface GamePlayer {
-    username: string;
-    rating: number;
-    result: 'win' | 'checkmated' | 'resigned' | 'repetition' | 'stalemate' | 'timeout' | 'insufficient' | 'abandoned';
+	username: string;
+	rating: number;
+	result:
+	'win' |
+	'checkmated' |
+	'resigned' |
+	'repetition' |
+	'stalemate' |
+	'timeout' |
+	'insufficient' |
+	'abandoned' |
+	'agreed' |
+	'timevsinsufficient';
 }
 
 export interface Game {
-    rules: 'chess' | string;
-    url: string;
-    white: GamePlayer;
-    black: GamePlayer;
-    time_class: 'blitz' | 'rapid' | 'daily';
-    time_control: string;
-    end_time: number;
-    rated: boolean;
+	rules: 'chess' | string;
+	url: string;
+	white: GamePlayer;
+	black: GamePlayer;
+	pgn: string;
+	fen: string;
+	time_class: 'blitz' | 'rapid' | 'daily';
+	time_control: string;
+	end_time: number;
+	rated: boolean;
 }
 
 // API responses
 export interface ArchivesListResponse {
-    archives: string[];
+	archives: string[];
 }
 
 export interface ArchiveGamesResponse {
-    games: Game[];
+	games: Game[];
 }
